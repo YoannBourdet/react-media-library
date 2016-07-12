7b016ba5dc91d9e45ac801a5e1a8e288eae04123
Yoann Bourdet <yoannbourdet@gmail.com>
2016-07-12 22:46:48 +0200

    Update medialibrary.jsx, modal.jsx

---
+++ app/js/components/Medialibrary.jsx
@@ -0,0 +1,79 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
+
+import Modal from './modal';
+
+export default class Medialibrary extends Component {
+
+  state = {
+    showMediaModal: false,
+    showFolderModal: false,
+  }
+
+  handleClickMedia() {
+    this.setState({ showMediaModal: !this.state.showMediaModal });
+  }
+
+  handleClickFolder() {
+    this.setState({ showFolderModal: !this.state.showFolderModal });
+  }
+
+  renderMediaModal() {
+    return(
+      <Modal
+        close={this.handleClickMedia.bind(this)}
+        isOpen={this.state.showMediaModal}
+      >
+        <h3>Media</h3>
+      </Modal>
+    );
+  }
+
+  renderFolderModal() {
+    return(
+      <Modal
+        close={this.handleClickFolder.bind(this)}
+        isOpen={this.state.showFolderModal}
+      >
+        <h3>Folder</h3>
+      </Modal>
+    );
+  }
+
+  render() {
+    return(
+      <div
+        className="layout"
+      >
+        <Row>
+          <Col md={4} xs={6}>
+            <div>
+              <h3>Folders</h3>
+              <Button
+                bsStyle="primary"
+                onClick={this.handleClickFolder.bind(this)}
+              >
+                Primary
+              </Button>
+            </div>
+          </Col>
+          <Col md={8} xs={12}>
+            <div>
+              <h3>Media Library</h3>
+              <Button
+                bsStyle="primary"
+                onClick={this.handleClickMedia.bind(this)}
+              >
+                Primary
+              </Button>
+            </div>
+          </Col>
+        </Row>
+        {this.renderFolderModal()}
+        {this.renderMediaModal()}
+      </div>
+    );
+  }
+}
