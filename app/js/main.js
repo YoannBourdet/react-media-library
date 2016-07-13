4f5089c3cdea22ff874c5765c6b95d6994a8a8c1
Yoann Bourdet <yoannbourdet@gmail.com>
2016-07-13 23:42:17 +0200

    add sass-loader

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 'use strict';
-
+/* styles */
+import '../scss/main';
+/* App */
 import React from 'react';
 import { render } from 'react-dom';
 import Medialibrary from './components/Medialibrary';
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,26 +2,29 @@
 import path from 'path';
 
 module.exports = {
-  context: __dirname,
+  context: path.join(__dirname, 'app'),
   entry: [
     'webpack-dev-server/client?http://localhost:8080/',
     'webpack/hot/dev-server',
-    './app/js/main.js',
+    './js/main.js',
   ],
   output: {
     path: path.join(__dirname, '/build/'),
     filename: 'medialibrary.js',
-    publicPath: '/build/',
+    publicPath: '/',
   },
   module: {
     loaders: [{
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       loaders: ['react-hot', 'babel'],
+    }, {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass'],
     }],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['', '.js', '.jsx', '.scss'],
     alias: {},
   },
 };
